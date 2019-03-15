24047c805de73349c0e77b8d9ba6b428a860fae3
guabadie <guillaume.abadie@capgemini.com>
2019-03-15 17:54:49 +0100

    Mise à jour README & ajout 1st script

--- GoogleSearch/README.md
+++ GoogleSearch/README.md
@@ -1,2 +1,12 @@
 GoogleSearch
 ============
+    /!\ activer l'env virtualisé GoogleSearch /!\
+    pip install requests
+    pip install bs4
+    
+    
+### USAGE
+Hard coded params:
+- research "Donald Duck" in google
+- get the first 5 results
+- save all HTML as "result.html"
---
+++ GoogleSearch/google_search_scrap.py
@@ -0,0 +1,73 @@
+import requests
+from bs4 import BeautifulSoup
+
+USER_AGENT = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
+
+
+def fetch_results(search_term, number_results, language_code):
+    assert isinstance(search_term, str), 'Search term must be a string'
+    assert isinstance(number_results, int), 'Number of results must be an integer'
+    escaped_search_term = search_term.replace(' ', '+')
+
+    google_url = 'https://www.google.com/search?q={}&num={}&hl={}'.format(escaped_search_term, number_results,
+                                                                          language_code)
+    response = requests.get(google_url, headers=USER_AGENT)
+    response.raise_for_status()
+
+    return search_term, response.text
+
+
+def parse_results(html, keyword):
+    soup = BeautifulSoup(html, 'html.parser')
+
+    found_results = []
+    rank = 1
+    result_block = soup.find_all('div', attrs={'class': 'g'})
+    for result in result_block:
+
+        link = result.find('a', href=True)
+        title = result.find('h3', attrs={'class': 'r'})
+        description = result.find('span', attrs={'class': 'st'})
+        if link and title:
+            link = link['href']
+            title = title.get_text()
+            if description:
+                description = description.get_text()
+            if link != '#':
+                found_results.append({'keyword': keyword, 'rank': rank, 'title': title, 'description': description})
+                rank += 1
+    return found_results
+
+
+def scrape_google(search_term, number_results, language_code):
+    try:
+        keyword, html = fetch_results(search_term, number_results, language_code)
+        results = parse_results(html, keyword)
+        return results
+    except AssertionError:
+        raise Exception("Incorrect arguments parsed to function")
+    except requests.HTTPError:
+        raise Exception("You appear to have been blocked by Google")
+    except requests.RequestException:
+        raise Exception("Appears to be an issue with your connection")
+
+
+def write_html(html):
+    f = open("result.html", "w+", encoding="utf-8")
+    f.write(html)
+    f.close
+
+
+def my_own_request():
+    my_url = "https://fr.wikipedia.org/wiki/Donald_Duck"
+    response = requests.get(my_url, headers=USER_AGENT)
+    response.raise_for_status()
+
+    return response.text
+
+
+if __name__ == '__main__':
+    keyword, html = fetch_results('donald duck', 5, 'fr')
+    #html = my_own_request()
+    write_html(html)
