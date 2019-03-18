3df83246a5dab701c1d249cf98c3ca3c19c2ad83
guabadie <guillaume.abadie@capgemini.com>
2019-03-18 17:44:02 +0100

    Ecriture dans un txt des urls d'images récupérées dans un json

--- GoogleImages/script_json_parsing.py
+++ GoogleImages/script_json_parsing.py
@@ -1,8 +1,16 @@
 import json
 
 def parse_json(datas):
+    i = 1
+    written_file = open("liste_des_urls", "w")
     for data in datas:
-        print(data['image_host'])
+        written_file.write("{}".format(i) + '\n')
+        written_file.write(data['image_host'] + '\n')
+        written_file.write(data['image_link'] + '\n')
+        written_file.write(data['image_source'] + '\n' + '\n')
+        i = i+1
+
+    written_file.close()
 
 with open('C:\\Workspace\\GoogleScrapping\\virtual_env\\scrapptest\\Scripts\\logs\\mimie mathy.json', 'r') as json_file:
 
